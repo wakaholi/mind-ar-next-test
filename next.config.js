f0be5b55560d6855098d75fa306532b30102d4ae
wakaholi <tao6128@gmail.com>
2024-01-16 19:13:17 +0900

    assetPrefixを設定

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["mind-ar"],
   output: "export",
+  assetPrefix: process.env.GITHUB_PAGES === "true" ? "/mind-ar-next-test" : "",
 };
 
 module.exports = nextConfig;
